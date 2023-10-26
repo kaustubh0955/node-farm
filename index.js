ba31d79ea3183b73ac5113fc70f8188beed76bfd
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-10-26 14:11:41 +0530

    dayy6

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+/////////////////////////////////////////////////////////////////
+//Files
+
 //Blocking,synchronous way
 //const input = fs.readFileSync("./txt/input.txt", "utf-8");
 //console.log(input);
 //const textOut = `This is what we know about the avocado: ${input}.\nCreated on ${Date.now()}`;
 //fs.writeFileSync(".txt/output.txt", textOut);
-console.log("File written");
+//console.log("File written");
 
 //Non Blocking ,synchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+/* fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
   if (err) return console.log("ERROR!");
 
   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
@@ -20,4 +26,27 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
     });
   });
 });
-console.log("Will read file!");
+console.log("Will read file!"); */
+/////////////////////////////////////////////////////////
+//SERVER
+const server = http.createServer((req, res) => {
+  const pathNmae = req.url;
+  if (pathNmae === "/" || pathNmae === "/overview") {
+    res.end("This is the Overview"); //sending response to the client
+  } else if (pathNmae === "/product") {
+    res.end("This is the Product");
+  } //api
+  else if (pathNmae === "/api") {
+    res.end("API");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    }); //Generating the error 404
+    res.end("<h1>Page Not Found!</h1>");
+  }
+}); //It'll accept a callback function
+//Incoming request
+server.listen(7000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
